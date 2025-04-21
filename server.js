2a3e847a963df4cafee3b02285f653e2bdc13ab1
Joe Kampf <108287201+jkampfwealthbox@users.noreply.github.com>
2025-04-21 15:00:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// /hello route: returns headers
 app.get('/hello', (req, res) => {
   res.set('Content-Type', 'application/json');
   res.send(JSON.stringify(req.headers, null, 2));
 });
 
+// Catch-all: redirect everything else to /hello
+app.use((req, res) => {
+  res.redirect('/hello');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
